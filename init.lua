826e85a3292b1a09636ca96dfbf758bf12e72762
torbato <giovanetorbato@outlook.com>
2025-05-15 20:41:01 +0200

    add relative and absolute lines in nvim

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = true
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -882,7 +882,7 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     --'folke/tokyonight.nvim',
-    
+
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -893,54 +893,54 @@ require('lazy').setup({
       --    comments = { italic = false }, -- Disable italics in comments
       --  },
       --}
-      require("catppuccin").setup({
-      	flavour = "auto", -- latte, frappe, macchiato, mocha
+      require('catppuccin').setup {
+        flavour = 'auto', -- latte, frappe, macchiato, mocha
         background = { -- :h background
-        	light = "latte",
-        	dark = "mocha",
+          light = 'latte',
+          dark = 'mocha',
         },
         transparent_background = false, -- disables setting the background color.
         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
         term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
-        	enabled = false, -- dims the background color of inactive window
-        	shade = "dark",
-        	percentage = 0.15, -- percentage of the shade to apply to the inactive window
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
         },
         no_italic = false, -- Force no italic
         no_bold = false, -- Force no bold
         no_underline = false, -- Force no underline
         styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        	comments = { "italic" }, -- Change the style of comments
-        	conditionals = { "italic" },
-        	loops = {},
-        	functions = {},
-        	keywords = {},
-        	strings = {},
-            	variables = {},
-           	numbers = {},
-            	booleans = {},
-        	properties = {},
-        	types = {},
-        	operators = {},
-        -- miscs = {}, -- Uncomment to turn off hard-coded styles
-    	},
-    	color_overrides = {},
-    	custom_highlights = {},
-    	default_integrations = true,
-    	integrations = {
-        	cmp = true,
-        	gitsigns = true,
-        	nvimtree = true,
-        	treesitter = true,
-        	notify = false,
-        	mini = {
-            		enabled = true,
-            		indentscope_color = "",
-        	},
-        	-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    	},
-      })
+          comments = { 'italic' }, -- Change the style of comments
+          conditionals = { 'italic' },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      }
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.

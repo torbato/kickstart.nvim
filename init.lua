308ff1d621e00f4f7d07c1f3c7b9fa00e9f45480
torbato <giovanetorbato@outlook.com>
2025-08-29 00:06:40 +0200

    fix colorscheme not loading

--- init.lua
+++ init.lua
@@ -906,6 +906,7 @@ require('lazy').setup({
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
       vim.g.sonokai_enable_italic = true
+      vim.g.sonokai_style = 'shusia'
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.

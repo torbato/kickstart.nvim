e3c105aa74026018f77702d6907c657c450a5e95
torbato <giovanetorbato@outlook.com>
2025-10-02 00:45:17 +0200

    add vimwiki

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,5 +4,16 @@
 -- See the kickstart.nvim README for more information
 return {
   { 'vuciv/golf' },
-  { 'vimwiki/vimwiki' },
+  {
+    'vimwiki/vimwiki',
+    init = function()
+      vim.g.vimwiki_list = {
+        {
+          path = '~/wiki',
+          syntax = 'markdown',
+          ext = 'md',
+        },
+      }
+    end,
+  },
 }
